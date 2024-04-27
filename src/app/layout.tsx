e5064d6cf931ba475faed53a6a6294b5b6e9e219
* <*>
2024-04-27 20:54:58 +0300

    feat(home-page): set the page's title to "3anki"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import '@mantine/core/styles.css';
 import React, { ReactElement } from 'react';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
+import { Metadata } from 'next';
 import { theme } from '@/theme';
 
-export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+export const metadata: Metadata = {
+  title: '3anki',
+  description: 'A new Notion to Anki converter',
 };
 
 export default function RootLayout({ children }: { children: any }): ReactElement {
